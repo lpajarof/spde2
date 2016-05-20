4719ad2d46ab619fd4e214d4e6547460a5698308
lpajaro <lpajaro@Leandro>
2016-05-19 23:04:14 -0500

    Commit 19/05/2016 11:00 pm

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -257,8 +257,7 @@ class AdminController extends Controller
        
        return new Response($v_estudiante);
     }
-    
-    
+       
     /**
      * Agrega estudiantes a la tabla estudiante
      * @Route("/admin/estudiantes/add", name="estudiantesAdd")
@@ -307,4 +306,51 @@ class AdminController extends Controller
         
     }
     
+    /**
+     * Modifica datos de estudiantes
+     * @Route("/admin/estudiantes/mod", name="estudiantesMod")
+     */
+    public function estudiantesMod(Request $request)
+    {
+        try{            
+            $em = $this->getDoctrine()->getManager();
+            
+            $v_programa = $em->getRepository('AppBundle:Programa')->find($request->request->get('programa'));
+            $v_tipoidentificacion = $em->getRepository('AppBundle:Tipoidentificacion')->find($request->request->get('tipoidentificacion'));
+            
+            $v_estudiante = $em->getRepository('AppBundle:Estudiante')->find($request->request->get('idestudiante'));
+            $v_estudiante->setCodigo($request->request->get('codigo'));
+            $v_estudiante->setPrograma($v_programa);
+            $v_estudiante->setTipoidentificacion($v_tipoidentificacion);
+            $v_estudiante->setIdentificacion($request->request->get('identificacion'));
+            $v_estudiante->setNombres($request->request->get('nombres'));
+            $v_estudiante->setApellidos($request->request->get('apellidos'));
+            $v_estudiante->setEmail($request->request->get('email'));
+            $v_estudiante->setTelefono($request->request->get('telefono'));
+            $v_estudiante->setDireccion($request->request->get('direccion'));
+            
+            $em->persist($v_estudiante);
+            $em->flush();
+
+            return new Response('<div class="alert alert-success alert-dismissible fade in" role="alert">'
+                                . 'Los datos han sido actualizados satisfactoriamente'
+                                . '</div>');
+            
+        } catch (\Exception $e) {
+            switch (get_class($e)) {
+                case 'Doctrine\DBAL\Exception\UniqueConstraintViolationException':
+                    return new Response('DBAL Exception<br/>');
+
+                case 'Doctrine\DBA\DBAException':
+                    return new Response('DBA Exception<br/>');               
+                default:
+                    return new Response($e->getMessage().'--'.get_class($e));                
+            }
+        }
+    }
+    
+    
+    /**
+     * Caracteristicas
+     */
 }
--- src/AppBundle/Repository/EstudianteRepository.php
+++ src/AppBundle/Repository/EstudianteRepository.php
@@ -19,14 +19,17 @@ class EstudianteRepository extends \Doctrine\ORM\EntityRepository
     {
         $arrEstudiante=$this->getEntityManager()
                                 ->createQuery('SELECT e.codigo'
+                                            . ',e.idestudiante'
                                             . ',ti.descripcion'
+                                            . ',ti.idtipoidentificacion'
+                                            . ',p.idprograma as programa' 
                                             . ',e.identificacion'
                                             . ',e.nombres'
                                             . ',e.apellidos'
                                             . ',e.direccion'
                                             . ',e.telefono'
                                             . ',e.email'
-                                            . ',p.descripcion as programa' 
+                                            . ',p.descripcion as nombreprograma' 
                                         . ' FROM AppBundle:Estudiante AS e '
                                         . ' JOIN AppBundle:Tipoidentificacion AS ti '
                                         . ' WITH e.tipoidentificacion = ti.idtipoidentificacion'
@@ -48,14 +51,19 @@ class EstudianteRepository extends \Doctrine\ORM\EntityRepository
             $arrJSON=$arrJSON.'"'.ucwords(strtolower($arrEstudiante[$i]["nombres"])).'",';
             $arrJSON=$arrJSON.'"'.ucwords(strtolower($arrEstudiante[$i]["apellidos"])).'",';
             $arrJSON=$arrJSON.'"'.$arrEstudiante[$i]["email"].'",';
-            $arrJSON=$arrJSON.'"'.$arrEstudiante[$i]["programa"].'",';
+            $arrJSON=$arrJSON.'"'.$arrEstudiante[$i]["nombreprograma"].'",';
             $arrJSON=$arrJSON.'" '
                     . '<button type=\"button\" id=\"btn\" class=\"btn btn-success btn-xs\" data-toggle=\"modal\" data-target=\"#vm_actualizar\"'
                     . 'data-codigo=\"'.$arrEstudiante[$i]["codigo"].'\"'
+                    . 'data-programa=\"'.$arrEstudiante[$i]["programa"].'\"'
+                    . 'data-tipoidentificacion=\"'.$arrEstudiante[$i]["idtipoidentificacion"].'\"'
+                    . 'data-identificacion=\"'.$arrEstudiante[$i]["identificacion"].'\"'
                     . 'data-nombres=\"'.$arrEstudiante[$i]["nombres"].'\"'
                     . 'data-apellidos=\"'.$arrEstudiante[$i]["apellidos"].'\"'
                     . 'data-email=\"'.$arrEstudiante[$i]["email"].'\"'
-                    . 'data-programa=\"'.$arrEstudiante[$i]["programa"].'\"'                    
+                    . 'data-telefono=\"'.$arrEstudiante[$i]["telefono"].'\"'
+                    . 'data-direccion=\"'.$arrEstudiante[$i]["direccion"].'\"'
+                    . 'data-idestudiante=\"'.$arrEstudiante[$i]["idestudiante"].'\"'
                     . '>'
                     . '<i class=\'glyphicon glyphicon-edit\'>'
                     . '</i> Modificar'
